dc8ff415b97d7c4e6cb880b8c8da8b188af73148
Felipe Costa <felipelena8@gmail.com>
2025-06-18 19:35:06 -0300

    Initial scaffolding

---
+++ src/main/java/com/uade/bookybe/BookyBeApplication.java
@@ -0,0 +1,13 @@
+package com.uade.bookybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookyBeApplication.class, args);
+    }
+
+}

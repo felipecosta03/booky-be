5eb2681028f758d8b5a957f7810cee0862f9177d
Felipe Costa <felipelena8@gmail.com>
2025-10-07 18:36:20 -0300

    Fix git push

--- src/main/java/com/uade/bookybe/config/LivekitConfig.java
+++ src/main/java/com/uade/bookybe/config/LivekitConfig.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @RequiredArgsConstructor
 public class LivekitConfig {
 
